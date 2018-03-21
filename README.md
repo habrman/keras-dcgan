ee73a67f3fa0b62896deb4d2d422ceae06785fcb
habrman <david.habrman@gmail.com>
2018-03-21 08:42:13 +0000

    Improve stability

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Train a deep convolutional generative adversarial network to generate mnist like
 * imageio
 
 ## How to
-Clone the repo and run `python3 mnist/mnist-gan.py`. This will create the output folder containing models and predictions for each epoch and tensorboard logs with generator and discriminator loss for each training step.
+Clone the repo and run `python3 mnist/train-mnist-gan.py`. This will create the output folder containing models and predictions for each epoch and tensorboard logs with generator and discriminator loss for each training step.
 
 There are input parameters for input size, batch size, number of training epochs and output directory that can be used.
 
 ## Network structure
-![alt text](https://github.com/habrman/keras-dcgan/blob/master/mnist/network-structure.png)
+![alt text](https://github.com/habrman/keras-dcgan/blob/master/assets/network-structure.png)
 
 ## MNIST training evolution
-![alt text](https://github.com/habrman/keras-dcgan/blob/master/mnist/mnist_training_example.gif)
+![alt text](https://github.com/habrman/keras-dcgan/blob/master/assets/mnist_training_example.gif)
--- mnist/train-mnist-gan.py
+++ mnist/train-mnist-gan.py
@@ -1,4 +1,4 @@
-from tensorflow.python.keras.layers import Conv2D, MaxPool2D, UpSampling2D, Conv2DTranspose, Input, Concatenate, Lambda, Dense, Reshape, BatchNormalization, Flatten
+from tensorflow.python.keras.layers import Conv2D, Input, Conv2DTranspose, Reshape, BatchNormalization, Flatten, Dense
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.datasets import mnist
 from tensorflow.python.keras.callbacks import TensorBoard
@@ -8,7 +8,15 @@ import numpy as np
 import argparse
 import imageio
 import glob
+import math
 import os
+
+
+# Custom loss for generator to avoid vanishing gradients when training generator
+def custom_loss():
+    def loss(y_true, y_pred):
+        return -tf.log(y_pred)
+    return loss
 
 
 def write_log(callback, names, logs, batch_no):
@@ -22,19 +30,13 @@ def write_log(callback, names, logs, batch_no):
 
 
 def create_generator(input_shape):
-    alpha = 2
-    base = 16
-
     input_vec = Input(input_shape, name='input')
     x = Dense(128*7*7, activation='tanh', name='dense')(input_vec)
     x = BatchNormalization()(x)
     x = Reshape((7, 7, 128))(x)
-    x = Conv2D(base * alpha**2, (3, 3), activation='relu', padding='same', name='conv_1')(x)
-    x = UpSampling2D()(x)
-    x = Conv2D(base * alpha, (3, 3), activation='relu', padding='same', name='conv_2')(x)
-    x = UpSampling2D()(x)
-    x = Conv2D(base, (3, 3), activation='relu', padding='same', name='conv_3')(x)
-    output = Conv2D(1, (3, 3), activation='tanh', padding='same', name='conv_4')(x)
+    x = Conv2DTranspose(128, (3, 3), strides=2, activation=tf.nn.leaky_relu, padding='same', name='de_conv_0')(x)
+    x = Conv2DTranspose(64, (3, 3), strides=2, activation=tf.nn.leaky_relu, padding='same', name='de_conv_1')(x)
+    output = Conv2D(1, (3, 3), activation='tanh', padding='same', name='output')(x)
 
     model = Model(input_vec, outputs=output)
 
@@ -42,20 +44,16 @@ def create_generator(input_shape):
 
 
 def create_discriminator(input_shape=(28, 28, 1)):
-    alpha = 2
-    base = 16
-
     input_vec = Input(input_shape, name='input')
-    x = Conv2D(base, (3, 3), activation='relu', padding='same', name='d_conv_1')(input_vec)
-    x = MaxPool2D((2, 2), padding='same')(x)
-    x = Conv2D(base * alpha, (3, 3), activation='relu', padding='same', name='d_conv_2')(x)
-    x = MaxPool2D((2, 2), padding='same')(x)
-    x = Conv2D(base * alpha**2, (3, 3), activation='relu', padding='same', name='d_conv_3')(x)
+    x = BatchNormalization()(input_vec)
+    x = Conv2D(16, (3, 3), strides=(2, 2), activation=tf.nn.leaky_relu, padding='same', name='conv_0')(x)
+    x = Conv2D(32, (3, 3), strides=(2, 2), activation=tf.nn.leaky_relu, padding='same', name='conv_1')(x)
+    x = Conv2D(64, (3, 3), strides=(2, 2), activation=tf.nn.leaky_relu, padding='same', name='conv_2')(x)
     x = Flatten()(x)
-    output = Dense(1, activation='sigmoid')(x)
+    output = Dense(1, activation='sigmoid', name='output')(x)
 
     model = Model(input_vec, outputs=output)
-    model.compile(loss='binary_crossentropy', optimizer='adam')
+    model.compile(loss='binary_crossentropy', optimizer='adadelta')
 
     return model
 
@@ -72,19 +70,19 @@ def create_gan(input_size):
 
     gan = Model(input_vec, outputs=output)
 
-    gan.compile(loss='binary_crossentropy', optimizer='adam')
+    gan.compile(loss=custom_loss(), optimizer='adadelta')
     return gan, discriminator, generator
 
 
-def create_random_vectors(samples, size, min=-1, max=1):
-    return np.random.uniform(min, max, size=(samples, size))
+def create_noise_batch(samples, size):
+    return np.random.normal(0, 0.2, size=(samples, size))
 
 
-def save_predictions(generator, epoch, input_size, pred_dir):
+def save_predictions(generator, epoch, step, input_size, pred_dir):
     img_size = 28
     sqrt_samples = 10
 
-    samples = create_random_vectors(sqrt_samples**2, input_size)
+    samples = create_noise_batch(sqrt_samples**2, input_size)
     pred = generator.predict_on_batch(samples)
 
     img = np.zeros((img_size*sqrt_samples, img_size*sqrt_samples, 1), dtype=np.uint8)
@@ -93,7 +91,7 @@ def save_predictions(generator, epoch, input_size, pred_dir):
             (i // sqrt_samples)*img_size: ((i // sqrt_samples)+1)*img_size,
             (i % sqrt_samples)*img_size: ((i % sqrt_samples) + 1)*img_size, :] = np.array((p * 127.5) + 127.5, dtype=np.uint8)
 
-    imageio.imwrite(os.path.join(pred_dir, 'pred%d.png' % epoch), img)
+    imageio.imwrite(os.path.join(pred_dir, 'pred%d-%d.png' % (epoch, step)), img)
     return
 
 
@@ -108,47 +106,54 @@ def train(input_size, batch_size, epochs, output_dir):
         os.makedirs(model_dir)
 
     gan, discriminator, generator = create_gan(input_size)
-    (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = mnist.load_data()
+    (x_train_real, _), _ = mnist.load_data()
 
     # Normalize mnist data between -1 and 1
-    x_train_mnist = np.expand_dims((x_train_mnist - 127.5) / 127.5, axis=-1)
-    x_test_mnist = np.expand_dims((x_test_mnist  - 127.5) / 127.5, axis=-1)
+    x_train_real = np.expand_dims((x_train_real - 127.5) / 127.5, axis=-1)
 
-    batch_size = batch_size // 2
+    batch_size = math.ceil(batch_size / 2)
     y_batch_ones_generator = np.ones([batch_size*2, 1])
     y_batch_zeros = np.zeros([batch_size, 1])
 
     callback = TensorBoard(log_dir=os.path.join(output_dir, 'log'))
     callback.set_model(gan)
 
+    indices = np.arange(x_train_real.shape[0])
+    np.random.shuffle(indices)
+    batch_indices = [indices[i:i + batch_size] for i in range(0, len(indices), batch_size)]
+
     step = 0
     for e in range(epochs):
         print('Epoch: %d/%d' % (e + 1, epochs))
 
-        for i in tqdm(range(x_test_mnist.shape[0] // batch_size)):
+        for indices in tqdm(batch_indices):
             # Create batch data
-            x_batch_mnist = x_train_mnist[i*batch_size:(i+1)*batch_size]
-            noise_batch = create_random_vectors(batch_size, input_size)
+            x_batch_real = x_train_real[indices]
+            noise_batch = create_noise_batch(len(indices), input_size)
             x_batch_generator = generator.predict_on_batch(noise_batch)
 
             # Train discriminator with label smoothing
-            y_batch_ones = create_random_vectors(batch_size, 1, min=0.55, max=1)
+            y_batch_ones = np.random.uniform(0.8, 1, size=(len(indices), 1))
 
-            discriminator_loss = discriminator.train_on_batch(
-                np.concatenate((x_batch_mnist, x_batch_generator)),
-                np.concatenate((y_batch_ones, y_batch_zeros)))
+            discriminator_loss_m = discriminator.train_on_batch(x_batch_real, y_batch_ones)
+            discriminator_loss_g = discriminator.train_on_batch(x_batch_generator, y_batch_zeros[:len(indices)])
+            discriminator_loss = (discriminator_loss_m + discriminator_loss_g) / 2
 
             # Train generator
-            noise_batch = create_random_vectors(batch_size * 2, input_size)
-            generator_loss = gan.train_on_batch(noise_batch, y_batch_ones_generator)
+            for _ in range(2):
+                noise_batch = create_noise_batch(len(indices) * 2, input_size)
+                generator_loss = gan.train_on_batch(noise_batch, y_batch_ones_generator[:2 * len(indices)])
             write_log(
                 callback, ['discriminator_train_loss', 'generator_train_loss'],
                 [discriminator_loss, generator_loss], step)
             step += 1
+            if step % 100 == 0:
+                save_predictions(generator, e, step, input_size, pred_dir)
+
         discriminator.save(os.path.join(model_dir, 'discriminator_%d.h5' % e))
         generator.save(os.path.join(model_dir, 'generator_%d.h5' % e))
 
-        save_predictions(generator, e, input_size, pred_dir)
+        save_predictions(generator, e, step, input_size, pred_dir)
     generate_gif(pred_dir, output_dir)
 
 
@@ -186,5 +191,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
-    train(
-        args.input_size, args.batch_size, args.epochs, args.output_dir)
+    train(args.input_size, args.batch_size, args.epochs, args.output_dir)
